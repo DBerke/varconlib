2665d6b0e10058d3d3ed69a5268bb6886cb10637
Daniel Berke <berke.daniel@gmail.com>
2018-08-20 15:18:20 +1000

    Added -t, -z, and -l flags to plotSpec.py. -t overplots a transmission spectrum from Kitt Peak showing atmospheric transmission. -z normalizes the plotted spectra by dividing them by their highest flux. -l takes a series of positions in wavelength space and plots dashed black lines at their locations, useful for marking the expected position of absorption lines.

--- plotSpec.py
+++ plotSpec.py
@@ -18,6 +18,9 @@ matplotlib.rc('xtick', labelsize=24)
 matplotlib.rc('ytick', labelsize=24)
 plt.rcParams['text.usetex'] = True
 
+# The Kit Peak transmission spectrum comes from Hinkle, Wallace, & Livingston,
+# 2003.
+# The solar spectrum comes from Reiners et al., 2015.
 
 # Start main body.
 parser = argparse.ArgumentParser(description='Plot HARPS spectra')
@@ -26,11 +29,9 @@ parser.add_argument('filenames', action='store', nargs='+',
 parser.add_argument('-o', '--name', action='store',
                     default='Spectrum.png',
                     help='Name of output file (in Pictures).')
-parser.add_argument('-n', '--minx', action='store', default=378,
-                    type=float,
+parser.add_argument('-n', '--minx', action='store', default=378, type=float,
                     help='The min x value (in nm).')
-parser.add_argument('-m', '--maxx', action='store', default=692,
-                    type=float,
+parser.add_argument('-m', '--maxx', action='store', default=692, type=float,
                     help='The max x value (in nm).')
 parser.add_argument('-i', '--miny', action='store', type=float,
                     help='The minimum y value.')
@@ -43,11 +44,17 @@ parser.add_argument('-b', '--boost', action='store', type=float, default=1,
 parser.add_argument('-v', '--vacuum', action='store_true',
                     help='Plot spectra in vacuum wavelengths.')
 parser.add_argument('-e', '--espresso', action='store_true',
-                   help='Plot an ESPRESSO file.')
+                    help='Plot an ESPRESSO file.')
 parser.add_argument('-u', '--unnormalized', action='store_true',
                     help='Plot the non-normalized solar flux.')
 parser.add_argument('-r', '--radvel', action='store', type=float,
                     help='Correct spectrum by given radial velocity. (km/s)')
+parser.add_argument('-t', '--transmission', action='store_true',
+                    help='Plot the Kitt Peak 2003 transmission spectrum.')
+parser.add_argument('-z', '--normalized', action='store_true',
+                    help='Plot the spectra normalized by their highest point.')
+parser.add_argument('-l', '--lines', nargs='+',
+                    help='Plot vertical lines at the given positions.')
 
 args = parser.parse_args()
 #args = parser.parse_args('-n 588.1 -m 588.7 -sb 50000\
@@ -61,22 +68,34 @@ args = parser.parse_args()
 outPicDir = "/Users/dberke/Pictures/"
 FITSfileDir = "/Volumes/External Storage/HARPS/"
 
-solarSpectrum = "/Users/dberke/Documents/IAG_solar_atlas_V1_405-1065.txt"
+solarSpectrum = "/Users/dberke/code/spectra/IAG_solar_atlas_V1_405-1065.txt"
+transSpectrum = "/Users/dberke/code/spectra/transdata_0.5_1_mic.txt"
 if args.sun:
     print('Importing IAG solar spectrum...', end='')
     wavenumber, sun_norm_flux, sun_flux = np.genfromtxt(solarSpectrum,
                                                         unpack=True)
     print('done.')
     print('Reversing solar wavelength array...', end='')
-    wl_sun_vac = np.flipud(np.array((1 / wavenumber) * 1e7)) # In nm here
+    wl_sun_vac = np.flipud(np.array((1 / wavenumber) * 1e7))  # In nm here
     print('reversed.')
     print('Reversing solar flux array...', end='')
     sol_norm_flux = np.flipud(np.array(sun_norm_flux))
     sol_flux = np.flipud(np.array(sun_flux))
     print('reversed.')
 
+if args.transmission:
+    print('Importing Kitt Peak transmission spectrum...', end='')
+    wavenumber_tr, trans_norm_flux = np.genfromtxt(transSpectrum, unpack=True)
+    print('done.')
+    print('Reversing KP wavelength array...', end='')
+    # Wavenumbers are given in vacuum, per the readme for the spectrum
+    wl_trans_vac = np.flipud(np.array((1 / wavenumber_tr) * 1e7))  # In nm here
+    print('reversed.')
+    print('Reversing KP flux array...', end='')
+    trans_norm_flux = np.flipud(np.array(trans_norm_flux))
+    print('done.')
 
-fig = plt.figure(figsize=(11,9))
+fig = plt.figure(figsize=(12,9))
 ax = fig.add_subplot(1, 1, 1)
 ax.set_xlabel("Wavelength (nm)", fontsize=20)
 ax.set_ylabel("Intensity", fontsize=20)
@@ -84,7 +103,7 @@ ax.set_ylabel("Intensity", fontsize=20)
 
 ax.grid(which='major', axis='both')
 ax.set_xlim(left=args.minx, right=args.maxx)
-if args.miny and args.maxy:
+if (args.miny and args.maxy) or args.maxy:
     ax.set_ylim(bottom=args.miny, top=args.maxy)
     print('Top and bottom set to {} and {}.'.format(args.miny, args.maxy))
 outfile = os.path.join(outPicDir, args.name)
@@ -101,6 +120,8 @@ for obj in args.filenames:
         else:
             spectrum = vcl.readHARPSfile(infile, obj=True)
         w = np.array(spectrum['w']) # In Angstroms here!
+        f = np.array(spectrum['f'])
+        e = spectrum['e']
         vac_wl = np.array(vcl.air2vacESO(w))
         if args.vacuum:
             wl = vac_wl / 10 # Convert to nm.
@@ -110,14 +131,20 @@ for obj in args.filenames:
             wl = vcl.lineshift(wl, -1*args.radvel)
         leftpos = vcl.wavelength2index(wl, args.minx)
         rightpos = vcl.wavelength2index(wl, args.maxx)
+        if args.normalized:
+            flux = f / f.max()
+            err = e / f.max()
+        else:
+            flux = f
+            err = e
         try:
-            maxflux = spectrum['f'][leftpos:rightpos].max()
+            maxflux = flux[leftpos:rightpos].max()
         except ValueError:
             print("Couldn't find a maximum flux in the given region.")
             print("Region exceeds the spectral array, try a smaller region?")
             maxflux = 1
         # Plot the spectrum
-        ax.errorbar(wl, spectrum['f'], yerr=spectrum['e'],
+        ax.errorbar(wl, flux, yerr=err,
                     linestyle='', marker='.', alpha=1,
                     label=spectrum['obj'])
 
@@ -149,8 +176,16 @@ if args.sun:
             linestyle='', marker='.', alpha=1,
             label='IAG Solar Spectrum')
 
+if args.transmission:
+    ax.plot(wl_trans_vac, trans_norm_flux, color='DarkCyan',
+            linestyle='', marker='.', alpha=1,
+            label='Kitt Peak transmission spectrum')
+
+if args.lines:
+    xpositions = [float(line) for line in args.lines]
+    ax.vlines(xpositions, 0, 1, transform=ax.get_xaxis_transform(),
+              color='black', linestyle='--', alpha=0.6)
+
 ax.legend(fontsize=24)
 fig.savefig(outfile, format='png')
 plt.close(fig)
-
-
