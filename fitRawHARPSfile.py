a19bf843e9a69ad76362f369bb0c80e4931fd0c4
Daniel Berke <berke.daniel@gmail.com>
2018-10-10 15:53:39 +1100

    Modified fitRawHARPSfile to use 4th instead of 5th order fits, and use a total of 20 points across the width of each CCD instead of 5.

--- fitRawHARPSfile.py
+++ fitRawHARPSfile.py
@@ -18,29 +18,54 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from numpy.polynomial.polynomial import polyfit
 import csv
-from adjustText import adjust_text
 
 
-def threshold(pixel, color):
+def threshold(ypixel, xpixel, color):
+    """Return a threshold above which to choose peak pixels based on position.
+
+    """
     if color is 'Red':
         return 10000
     elif color is 'Blue':
-        if 0 < pixel <= 100:
-            return 340
-        elif 100 < pixel <= 105:
-            return 410
-        elif 105 < pixel <= 125:
-            return 600
-        elif 125 < pixel <= 400:
-            return 500
-        elif 400 < pixel <= 800:
-            return 900
-        elif 800 < pixel <= 1400:
-            return 3400
-        elif 1400 < pixel < 2080:
-            return 3500
+        if 1100 < xpixel < 3100:
+            if 0 < ypixel <= 100:
+                return 500
+            elif 100 < ypixel <= 107:
+                if xpixel == 1509:
+                    return 4100
+                else:
+                    return 3100
+            elif 107 < ypixel <= 120:
+                return 1000
+            elif 120 < ypixel <= 125:
+                return 1200
+            elif 125 < ypixel <= 400:
+                return 1300
+            elif 400 < ypixel <= 800:
+                return 1400
+            elif 800 < ypixel <= 1400:
+                return 3400
+            elif 1400 < ypixel < 2080:
+                return 3500
+            else:
+                return 400
         else:
-            return 400
+            if 0 < ypixel <= 100:
+                return 340
+            elif 100 < ypixel <= 105:
+                return 410
+            elif 105 < ypixel <= 125:
+                return 550
+            elif 125 < ypixel <= 400:
+                return 500
+            elif 400 < ypixel <= 800:
+                return 900
+            elif 800 < ypixel <= 1400:
+                return 3400
+            elif 1400 < ypixel < 2080:
+                return 3500
+            else:
+                return 400
 
 
 filepath = Path('/Users/dberke/HARPS/HARPS.2017-01-01T20_20_49.449.fits')
@@ -55,8 +80,8 @@ arrays = (bluedata, reddata)
 colors = ('Blue', 'Red')
 peaklists = (bluepeaks, redpeaks)
 
-slicepoints = [int(n) for n in np.linspace(1023, 3071, num=20, endpoint=True)]
-slicepoints = (4, 1023, 2047, 3071, 4091)
+slicepoints = [int(n) for n in np.linspace(4, 4091, num=20, endpoint=True)]
+#slicepoints = (4, 1023, 2047, 3071, 4091)
 for array, color, peaklist in zip(arrays, colors, peaklists):
     for i in slicepoints:
         # Take a thin slice of the array around each of the points,
@@ -68,7 +93,7 @@ for array, color, peaklist in zip(arrays, colors, peaklists):
         for j in range(len(data_slice)):
             if (j == 0) or (j == len(data_slice)-1):
                 continue
-            if data_slice[j] > threshold(j, color):
+            if data_slice[j] > threshold(j, i, color):
                 if (data_slice[j-1] < data_slice[j]) and\
                    (data_slice[j+1] < data_slice[j]):
                     peaks.append(j)
@@ -81,31 +106,32 @@ for array, color, peaklist in zip(arrays, colors, peaklists):
 
         ax.plot(data_slice, marker='.', markersize=4, linestyle='-',
                 color='Gray')
-        ax.plot(peaks, data_slice[peaks], marker='.', markersize=9,
+        ax.plot(peaks, data_slice[peaks], marker='.', markersize=12,
                 color=color, markeredgecolor='Black', markeredgewidth=1,
                 linestyle='')
         xrange = np.array([x for x in range(0, 4095, 2)])
-        y = [threshold(x, color) for x in xrange]
+        y = [threshold(j, i, color) for j in xrange]
         ax.plot(xrange, y, color=color,
                 linestyle='-', linewidth=1, marker='')
 
-        outfile = '/Users/dberke/Pictures/Fit_{0}_{1}.png'.format(color, i)
+        outfile = '/Users/dberke/Pictures/Fitting_CCDs/Fit_{0}_{1}.png'.\
+                  format(color, i)
         fig.savefig(outfile)
         plt.close(fig)
 
         slicelist = []
 
         if color == 'Red':
-            for k in range(0, len(peaks), 2):
-                slicelist.append((i, peaks[k]))
+            start_on = 0
         elif color == 'Blue':
-            if (i == 4) or (i == 4091):
-                for k in range(0, len(peaks), 2):
-                    slicelist.append((i, peaks[k]))
+            if 860 < i < 3650:
+                start_on = 1
             else:
-                for k in range(1, len(peaks), 2):
-                    slicelist.append((i, peaks[k]))
+                start_on = 0
+        for k in range(start_on, len(peaks), 2):
+            slicelist.append((i, peaks[k]))
         peaklist.append(slicelist)
+print('Created individual slice plots.')
 
 plotrange = np.linspace(0, 4096, num=100)
 #plotrange = slicepoints
@@ -121,7 +147,7 @@ for peaklist, color, array in zip(peaklists, colors, arrays):
     for points in slices:
         x = [point[0] for point in points]
         y = [point[1] for point in points]
-        coeffs = polyfit(x, y, 5)
+        coeffs = polyfit(x, y, 4)
         coeffslist.append(coeffs)
         ax2.plot(x, y, marker='+', color='Cyan', markersize=8, alpha=1,
                  linestyle='')
@@ -134,14 +160,13 @@ for peaklist, color, array in zip(peaklists, colors, arrays):
                  linewidth=1, linestyle='--', color='White',
                  alpha=1)
 
-#    adjust_text(labels)
-    outfile = '/Users/dberke/Pictures/Fit_{}.png'.format(color)
+    outfile = '/Users/dberke/Pictures/Fitting_CCDs/Fit_{}.png'.format(color)
     fig2.savefig(outfile)
     plt.close(fig2)
 
-    print(coeffslist)
     datafile = 'data/HARPS_Fit_Coeffs_{}.txt'.format(color)
     with open(datafile, 'w', newline='') as f:
         csvwriter = csv.writer(f, delimiter=',')
         for item in coeffslist:
             csvwriter.writerow(item)
+print('Created CDD overplots.')
