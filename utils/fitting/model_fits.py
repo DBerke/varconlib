bac15371ed8533d9317e8db086d208847d460580
Daniel Berke <berke.daniel@gmail.com>
2019-03-27 11:58:15 +1100

    Added 'Gaussian' to 'AbsorptionFeatureFit' name, merged fitting code into __init__.
    Added more code to duplicate functionality from old fitGaussian function in
    varconlib; the code now performs the fit, returns and parses the results,
    calculates the errors, and saves a bunch of information relating to the fit.
    
    The plotting code has also been separated into a new plotFit method for better
    encapsulation.

--- utils/fitting/model_fits.py
+++ utils/fitting/model_fits.py
@@ -9,6 +9,7 @@ Code to define a class for a model fit to an absorption line.
 
 """
 
+import configparser
 from pathlib import Path
 import numpy as np
 from scipy.optimize import curve_fit, OptimizeWarning
@@ -16,15 +17,19 @@ import unyt as u
 from tqdm import tqdm
 import varconlib as vcl
 
+config_file = Path('/Users/dberke/code/config/variables.cfg')
+config = configparser.ConfigParser(interpolation=configparser.
+                                   ExtendedInterpolation())
+config.read(config_file)
 
-class AbsorptionFeatureFit(object):
+
+class AbsorptionFeatureFitGaussian(object):
     """A class to fit an absorption line and store information about the fit.
 
     """
 
     def __init__(self, transition, observation, verbose=False):
-        """Construct a fit to an absorption feature using the given model
-        function.
+        """Construct a fit to an absorption feature using a gaussian.
 
         Parameters
         ----------
@@ -40,118 +45,190 @@ class AbsorptionFeatureFit(object):
             the function.
 
         """
+
         self.transition = transition
         self.observation = observation
-        self.verbose = verbose
-        self.fitAbsorptionFeature()
-
-    def fitAbsorptionFeature(self):
-        """Find and fit an absorption feature created by the transition given
-        during initialization.
-
-        """
 
         # Define some useful numbers and variables
 
-        # The ranges in velocity space to search around to find the nadir of
+        # The ranges in velocity space to search around to find the minimum of
         # an absorption line.
         search_range_vel = 5 * u.km / u.s
+
         # The range in velocity space to consider to find the continuum.
         continuum_range_vel = 25 * u.km / u.s
-        # The number of pixels either side of the feature nadir to use in the
+
+        # The number of pixels either side of the flux minimum to use in the
         # fit.
         pixel_range = 3
 
-        radial_velocity = self.observation._radialVelocity
+        radial_velocity = self.observation.radialVelocity
         # Shift the wavelength being searched for to correct for the radial
         # velocity of the star.
-        rv_fixed_wavelength = vcl.shift_wavelength(self.transition.wavelength,
-                                                   radial_velocity)
-        rv_fixed_wavelength.convert_to_units(u.nm)
-        tqdm.write('Given radial velocity {}, line {} should be at {:.4f}'.
+        nominal_wavelength = self.transition.wavelength
+        self.correctedWavelength = vcl.shift_wavelength(nominal_wavelength,
+                                                        radial_velocity)
+        self.correctedWavelength.convert_to_units(u.nm)
+        tqdm.write('Given RV {:.2f}: line {:.4f} should be at {:.4f}'.
                    format(radial_velocity,
-                          self.transition.wavelength,
-                          rv_fixed_wavelength))
-        order = self.observation.findWavelength(rv_fixed_wavelength)
+                          nominal_wavelength.to(u.nm),
+                          self.correctedWavelength.to(u.nm)))
+        # Find which order of the echelle spectrum the wavelength falls in.
+        order = self.observation.findWavelength(self.correctedWavelength,
+                                                mid_most=True)
 
-        wavelengthArray = self.observation._barycentricArray[order]
-        fluxArray = self.observation._photonFluxArray[order]
-        errorArray = self.observation._errorArray[order]
+        baryArray = self.observation.barycentricArray[order]
+        fluxArray = self.observation.photonFluxArray[order]
+        errorArray = self.observation.errorArray[order]
 
         # Figure out the range in wavelength space to search around the nominal
-        # wavelength for the absorption nadir, as well as the range to take for
+        # wavelength for the flux minimum, as well as the range to take for
         # measuring the continuum.
-        search_range = vcl.get_wavelength_separation(search_range_vel,
-                                                     rv_fixed_wavelength)
-        continuum_range = vcl.get_wavelength_separation(continuum_range_vel,
-                                                        rv_fixed_wavelength)
+        search_range = vcl.velocity2wavelength(search_range_vel,
+                                               self.correctedWavelength)
+        continuum_range = vcl.velocity2wavelength(continuum_range_vel,
+                                                  self.correctedWavelength)
 
-        low_search_index = vcl.wavelength2index(rv_fixed_wavelength -
+        low_search_index = vcl.wavelength2index(self.correctedWavelength -
                                                 search_range,
-                                                wavelengthArray)
-        high_search_index = vcl.wavelength2index(rv_fixed_wavelength +
+                                                baryArray)
+        high_search_index = vcl.wavelength2index(self.correctedWavelength +
                                                  search_range,
-                                                 wavelengthArray)
-        low_continuum_index = vcl.wavelength2index(rv_fixed_wavelength -
+                                                 baryArray)
+        low_continuum_index = vcl.wavelength2index(self.correctedWavelength -
                                                    continuum_range,
-                                                   wavelengthArray)
-        high_continuum_index = vcl.wavelength2index(rv_fixed_wavelength +
+                                                   baryArray)
+        high_continuum_index = vcl.wavelength2index(self.correctedWavelength +
                                                     continuum_range,
-                                                    wavelengthArray)
+                                                    baryArray)
         central_index = low_search_index + \
             fluxArray[low_search_index:high_search_index].argmin()
 
-        continuum_level = fluxArray[low_continuum_index:high_continuum_index]\
-            .max()
+        self.continuum_level = fluxArray[low_continuum_index:
+                                         high_continuum_index].max()
 
-        line_nadir = fluxArray[central_index]
+        flux_minimum = fluxArray[central_index]
 
         low_fit_index = central_index - pixel_range
         high_fit_index = central_index + pixel_range + 1
 
-        wavelengths = wavelengthArray[low_fit_index:high_fit_index]
-        fluxes = fluxArray[low_fit_index:high_fit_index]
-        errors = errorArray[low_fit_index:high_fit_index]
+        # Grab the wavelengths, fluxes, and errors from the region to be fit.
+        self.wavelengths = baryArray[low_fit_index:high_fit_index]
+        self.fluxes = fluxArray[low_fit_index:high_fit_index]
+        self.errors = errorArray[low_fit_index:high_fit_index]
 
-        line_depth = continuum_level - line_nadir
+        line_depth = self.continuum_level - flux_minimum
 
-        initial_guess = (line_depth * -1,
-                         rv_fixed_wavelength.to(u.angstrom).value,
-                         0.08)
+        self.initial_guess = (line_depth * -1,
+                              self.correctedWavelength.to(u.angstrom).value,
+                              0.08)
 
         tqdm.write('Attempting to fit line at {:.4f} with initial guess:'.
-                   format(rv_fixed_wavelength))
-        print(initial_guess)
+                   format(self.correctedWavelength))
+        if verbose:
+            print('Initial parameters are:\n{}\n{}\n{}'.format(
+                  *self.initial_guess))
+
+        # Do the fitting:
         try:
-            popt, pcov = curve_fit(vcl.gaussian, wavelengths.value,
-                                   fluxes - continuum_level,
-                                   sigma=errors, absolute_sigma=True,
-                                   p0=initial_guess, method='lm')
+            self.popt, self.pcov = curve_fit(vcl.gaussian,
+                                             self.wavelengths.value,
+                                             self.fluxes -
+                                             self.continuum_level,
+                                             sigma=self.errors,
+                                             absolute_sigma=True,
+                                             p0=self.initial_guess,
+                                             method='lm')
         except (OptimizeWarning, RuntimeError):
-            pass
-        print(continuum_level)
-        print(line_nadir)
-        print(wavelengths)
-#        print(initial_guess)
+            raise
+
+        if verbose:
+            print(self.popt)
+            print(self.pcov)
+
+        # Recover the fitted values for the parameters:
+        self.amplitude = self.popt[0]
+        self.centroid = self.popt[1] * u.angstrom
+        self.standardDev = self.popt[2] * u.angstrom
+
+        # Find 1-σ errors from the covariance matrix:
+        self.perr = np.sqrt(np.diag(self.pcov))
+
+        self.amplitudeErr = self.perr[0]
+        self.centroidErr = self.perr[1] * u.angstrom
+        self.standardDevErr = self.perr[2] * u.angstrom
+
+        # Compute the χ^2 value:
+        residuals = (self.fluxes - self.continuum_level) - \
+            vcl.gaussian(self.wavelengths.value, *self.popt)
+
+        self.chiSquared = sum((residuals / self.errors) ** 2)
+        self.chiSquaredNu = self.chiSquared / 4  # ν = 7 (pixels) - 3 (params)
+        if (self.chiSquaredNu > 1):
+            self.centroidErr *= np.sqrt(self.chiSquaredNu)
+
+        # Find the full width at half max:
+        self.FWHM = 2 * np.sqrt(2 * np.log(2)) * self.standardDev
+        self.FWHMErr = 2 * np.sqrt(2 * np.log(2)) * self.standardDevErr
+        self.velocityFWHM = vcl.wavelength2velocity(self.centroid,
+                                                    self.centroid +
+                                                    self.FWHM / 2)
+        # TODO: Error for FWHM velocity.
+
+        # Compute the offset between the input wavelength and the wavelength
+        # found in the fit.
+        self.offset = self.centroid - self.correctedWavelength
+        self.velocityOffset = vcl.wavelength2velocity(self.centroid,
+                                                      self.correctedWavelength)
+        # TODO: Error for velocity offset.
+
+        if verbose:
+            print(self.continuum_level)
+            print(flux_minimum)
+            print(self.wavelengths)
+
+    def toVelocity(self, attribute):
+        wavelength1 = attribute
+        try:
+            wavelength2 = getattr(self, attribute.__name__ + 'Err')
+        except KeyError:
+            raise KeyError('No appropriately named "Err" attribute found.')
+        return vcl.get_velocity_offset(wavelength1, wavelength2)
+
+    def plotFit(self, outfile=None):
         import matplotlib.pyplot as plt
+        # Set up the figure.
         fig = plt.figure(figsize=(8, 8))
         ax = fig.add_subplot(1, 1, 1)
-        ax.errorbar(wavelengths, fluxes-continuum_level, yerr=errors,
-                    color='blue', marker='o', linestyle='')
-        ax.plot(wavelengths, (vcl.gaussian(wavelengths.value, *initial_guess)),
-                color='Black')
-        ax.plot(wavelengths, (vcl.gaussian(wavelengths.value, *popt)),
-                color='Red', linestyle='--')
-        outfile = Path('/Users/dberke/Pictures/debug_norm.png')
+
+        # Begin plotting.
+        ax.axvline(self.correctedWavelength.to(u.angstrom),
+                   color='SeaGreen', linestyle='--',
+                   label='RV-corrected transition λ',)
+        ax.axvline(self.centroid.to(u.angstrom),
+                   color='PaleVioletRed', label='Fitted centroid',
+                   linestyle='-')
+        ax.errorbar(self.wavelengths.to(u.angstrom),
+                    self.fluxes-self.continuum_level,
+                    yerr=self.errors,
+                    color='SaddleBrown', marker='o', linestyle='',
+                    ecolor='Peru', label='Data')
+        ax.plot(self.wavelengths.to(u.angstrom),
+                (vcl.gaussian(self.wavelengths.value, *self.initial_guess)),
+                color='DeepSkyBlue', label='Initial guess',
+                linestyle='--')
+        x = np.linspace(self.wavelengths.to(u.angstrom).min().value,
+                        self.wavelengths.to(u.angstrom).max().value, 40)
+        ax.plot(x, (vcl.gaussian(x, *self.popt)), color='Olive',
+                linestyle='-', label='Fitted Gaussian')
+
+        ax.legend()
+        # Save the resultant plot.
+        if outfile is None:
+            stars_dir = Path(config['PATHS']['stars_dir'])
+            outfile = stars_dir /\
+                'Transition_{:.3f}.png'.format(
+                        self.transition.wavelength.to(u.angstrom).value)
         print('Created plot at {}'.format(outfile))
         fig.savefig(str(outfile))
         plt.close(fig)
-#            raise
-
-        print(popt)
-        print(pcov)
-
-# find order where transition is
-#   localize wavelength, find limits of WFE arrays
-# fit absorption feature (normalize first?)
-# store information about fit
