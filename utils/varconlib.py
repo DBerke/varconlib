ab592c6687e82f56b12402dc9401d5fb9a8c0bbb
Daniel Berke <berke.daniel@gmail.com>
2019-07-25 11:23:53 +1000

    Fixed LaTeX formatting of equation.

--- utils/varconlib.py
+++ utils/varconlib.py
@@ -302,7 +302,7 @@ def integrated_gaussian(pixel, amplitude, mu, sigma, baseline):
     tuple in `pixel`).
 
     The function is given by
-    .. math::    f(x1, x2) = \sqrt{\frac{\tau}{4}} A\
+    .. math::    f(x_1, x_2) = \sqrt{\frac{\tau}{4}} A\
                  \sigma\left[\erf\left(\frac{x_{2}-\mu}{\sqrt{2}\sigma}\right)\
                  -\erf\left(\frac{x_{1}-\mu}{\sqrt{2}\sigma}\right)\right]\
                  -D x_{1}+D x_{2}
