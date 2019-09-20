b79ce15cddad95f023c1301877cf205974ea1ac6
Daniel Berke <berke.daniel@gmail.com>
2019-09-20 12:10:32 +1000

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 20 11:41:45 2019
+
+@author: dberke
+
+setup.py file.
+"""
+
+from setuptools import setup, find_packages
+
+setup(name="varconlib", packages=find_packages())
