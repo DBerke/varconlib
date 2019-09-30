2f76af3f32afc0c2caaf38e47f6644116b18b559
Daniel Berke <berke.daniel@gmail.com>
2019-09-30 15:35:14 +1000

    Added pickle files of final selection of transitions and pairs to varconlib namespace.

--- varconlib/__init__.py
+++ varconlib/__init__.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 
 __all__ = ['base_dir', 'data_dir', 'masks_dir', 'pickle_dir',
-           'pixel_geom_files_dir']
+           'pixel_geom_files_dir', 'final_selection_file',
+           'final_pair_selection_file']
 
 
 # Define some important paths to be available globally relative to the
@@ -29,6 +30,12 @@ masks_dir = data_dir / 'masks'
 pickle_dir = data_dir / 'pickles'
 
 pixel_geom_files_dir = data_dir / 'pixel_geom_files'
+
+# The pickle file containing the final selection of transitions to use.
+final_selection_file = pickle_dir / 'final_transitions_selection.pkl'
+
+# The pickle file containing the final selection of pairs to use.
+final_pair_selection_file = pickle_dir / 'final_pairs_selection.pkl'
 
 
 # Read the config file to get values from it.
