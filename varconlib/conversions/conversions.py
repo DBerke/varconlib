ef4cb2ef45c5c45ec57d02237f48af71ba6d9ba6
Daniel Berke <berke.daniel@gmail.com>
2019-10-02 11:23:26 +1000

    Improved test coverage.

--- varconlib/conversions/conversions.py
+++ varconlib/conversions/conversions.py
@@ -63,11 +63,11 @@ def vac2airESO(ll):
 
     """
 
-    if type(ll) in (u.array.unyt_quantity, u.array.unyt_array):
+    if isinstance(ll, (u.array.unyt_quantity, u.array.unyt_array)):
         original_units = ll.units
         ll.convert_to_units(u.angstrom)
         ll = ll.value
-    elif type(ll) in (float, np.ndarray):
+    elif isinstance(ll, (float, np.ndarray)):
         original_units = 1
 
     llair = ll/air_indexEdlen53(ll)
@@ -133,9 +133,6 @@ def air2vacESO(air_wavelengths_array, verbose=False):
     vacuum_array = u.unyt_array(vacuum_wavelengths_list, u.angstrom)
 
     if reshape:
-        if verbose:
-            tqdm.write('Converting back to original shape: {}.'.format(
-                       original_shape))
         # Reshape the array back to its original shape.
         return vacuum_array.reshape(original_shape).to(original_units)
     else:
@@ -167,26 +164,3 @@ def air2vacMortonIAU(wl_air):
     n = 1 + 0.00008336624212083 + (0.02408926869968 / (130.1065924522 - s**2))\
         + (0.0001599740894897 / (38.92568793293 - s**2))
     return wl_air * n
-
-
-def vac2airPeckReeder(wl_vac):
-    """
-    Return the air wavelength of a vacuum wavelength using the formula from
-    Peck & Reeder 1972.
-
-    Parameters
-    ----------
-    wl_vac : unyt quantity with dimensions length
-        The wavelength to convert from vacuum to air. The Peck & Reeder formula
-        itself uses values in reciprocal micrometers.
-
-    Formula taken from Peck & Reeder, J. Opt. Soc. Am. 62, 958 (1972).
-    https://www.osapublishing.org/josa/fulltext.cfm?uri=josa-62-8-958&id=54743
-
-    """
-    # TODO: Figure out how this ever worked, when it needs reciprocal
-    # micrometers. Why did I add it in the first place?
-    s = wl_vac.to(u.um).value
-    n = 1 + ((8060.51 + (2480990 / (132.274 - s**2)) + (17455.7 / (39.32457 -
-             s**s))) / 1e8)
-    return wl_vac / n
--- varconlib/conversions/tests/test_conversions.py
+++ varconlib/conversions/tests/test_conversions.py
@@ -13,36 +13,70 @@ import pytest
 
 import unyt as u
 
-from varconlib.conversions import (air_indexEdlen53, vac2airESO, air2vacESO)
+from varconlib.conversions import (air_indexEdlen53, vac2airESO, air2vacESO,
+                                   vac2airMorton00, air2vacMortonIAU)
 
 
 @pytest.fixture(scope='module')
 def vacuum_array():
-    return u.unyt_array(range(380, 690, 1), units=u.nm)
+    return u.unyt_array(range(3800, 6900, 10), units=u.angstrom)
 
 
 @pytest.fixture(scope='module')
 def air_array_2D():
-    return u.unyt_array(range(500, 600, 1), units=u.nm).reshape(10, 10)
+    return u.unyt_array(range(5000, 6000, 10), units=u.angstrom).reshape(10,
+                                                                         10)
 
 
 @pytest.fixture(scope='module')
 def air_array():
-    return u.unyt_array(range(380, 690, 1), units=u.nm)
+    return u.unyt_array(range(3800, 6900, 10), units=u.angstrom)
+
+
+@pytest.fixture(scope='module')
+def unitless_array():
+    return np.array([5000, 5001, 5002, 5003, 5004])
 
 
 class TestEdlen1953(object):
+
     def testAirIndex(self):
-        assert air_indexEdlen53(500) == pytest.approx(0.99994748)
+        assert air_indexEdlen53(5000) == pytest.approx(1.0002789636500335)
 
     def testAirUnits(self, vacuum_array):
-        assert vac2airESO(vacuum_array).units == u.nm
+        assert vac2airESO(vacuum_array).units == u.angstrom
 
     def test2DVacuumUnits(self, air_array_2D):
-        assert air2vacESO(air_array_2D).units == u.nm
+        assert air2vacESO(air_array_2D).units == u.angstrom
 
-    def testReshapeArrays(self, air_array_2D):
+    def testReshapeArrays(self, air_array_2D, verbose=True):
+        assert air_array_2D.ndim == 2
         assert np.shape(air2vacESO(air_array_2D)) == (10, 10)
 
     def testVacuumUnits(self, air_array):
-        assert air2vacESO(air_array).units == u.nm
+        assert air2vacESO(air_array, verbose=True).units == u.angstrom
+
+    def testNoUnits(self, unitless_array):
+
+        assert vac2airESO(unitless_array) == pytest.approx([4998.60557074,
+                                                            4999.60530505,
+                                                            5000.60503934,
+                                                            5001.60477364,
+                                                            5002.60450793])
+
+
+class TestMorton2000(object):
+
+    def testVac2Air(self, unitless_array):
+        assert vac2airMorton00(unitless_array) == pytest.approx([4998.605522,
+                                                                 4999.605256,
+                                                                 5000.604990,
+                                                                 5001.604724,
+                                                                 5002.604459])
+
+    def testAir2Vac(self, unitless_array):
+        assert air2vacMortonIAU(unitless_array) == pytest.approx([5001.394848,
+                                                                  5002.395114,
+                                                                  5003.395380,
+                                                                  5004.395646,
+                                                                  5005.395911])
