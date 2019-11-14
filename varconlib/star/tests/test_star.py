ba2f394a53b62bded51a9dff58671d127c802727
Daniel Berke <berke.daniel@gmail.com>
2019-11-14 14:23:30 +1100

    Added some tests to improve coverage, changed some names based on changes to star.py

--- varconlib/star/tests/test_star.py
+++ varconlib/star/tests/test_star.py
@@ -18,6 +18,11 @@ import unyt as u
 import varconlib as vcl
 from varconlib.star import Star
 
+# Tell pytest to ignore SerializedWarnings that pop up from storing bidicts
+# in an HDF5 file.
+pytestmark = pytest.mark.filterwarnings("ignore::hickle.hickle."
+                                        "SerializedWarning")
+
 
 base_test_dir = vcl.data_dir / f'spectra/HD117618'
 
@@ -43,46 +48,68 @@ class TestStar(object):
 
     @pytest.fixture(scope='class')
     def test_star(self, test_dir):
-        return Star('HD117618', star_dir=test_dir, suffix='int')
+        return Star('HD117618', star_dir=test_dir, suffix='int',
+                    load_data=False)
 
     def testNonExistentDir(self):
         with pytest.raises(RuntimeError):
             Star('HD117618', star_dir='/nonsensical_dir_that_should_not_exist',
                  suffix='int')
 
+    def testIndexMethods(self):
+        s = Star('HD1111')
+        with pytest.raises(AttributeError):
+            s.p_index('')
+        with pytest.raises(AttributeError):
+            s.t_index('')
+        with pytest.raises(KeyError):
+            s.od_index('')
+
     def testLabelMethods(self):
         s = Star('HD1111')
         with pytest.raises(AttributeError):
-            s._p_label('')
+            s.p_label('')
         with pytest.raises(AttributeError):
-            s._t_label('')
+            s.t_label('')
         with pytest.raises(KeyError):
-            s._o_label('')
+            s.od_date('')
 
     def testName(self, test_star):
-
         assert test_star.name == 'HD117618'
 
-    def testArrayShapes(self, test_star):
+    def testFiberSplitIndex(self, test_star):
+        assert test_star.fiberSplitIndex is None
 
+    def testArrayShapes(self, test_star):
         assert test_star.fitMeansArray.shape == (3, 184)
         assert test_star.fitErrorsArray.shape == (3, 184)
         assert test_star.pairSeparationsArray.shape == (3, 284)
         assert test_star.pairSepErrorsArray.shape == (3, 284)
 
-    def testTransitionDict(self, test_star):
+    def testPairBidict(self, test_star):
+        assert test_star.p_index('4217.791Fe1_4219.893V1_16') == 0
+        assert test_star.p_label(283) == '6774.190Ni1_6788.733Fe1_70'
 
-        assert test_star._t_label('4217.791Fe1_16') == 0
-        assert test_star._o_label('2005-05-02T03:49:08.735') == 0
-        assert test_star._o_label(dt.datetime(year=2005, month=5,
+    def testTransitionBidict(self, test_star):
+        assert test_star.t_index('4217.791Fe1_16') == 0
+        assert test_star.t_label(183) == '6788.733Fe1_70'
+
+    def testObsevationDateBidict(self, test_star):
+        assert test_star.od_index('2005-05-02T03:49:08.735') == 0
+        assert test_star.od_index(dt.datetime(year=2005, month=5,
                                               day=2, hour=3,
                                               minute=49, second=8,
                                               microsecond=735000)) == 0
         with pytest.raises(KeyError):
-            test_star._o_label(dt.datetime(year=2000, month=1,
+            test_star.od_index(dt.datetime(year=2000, month=1,
                                            day=1, hour=0,
                                            minute=0, second=0))
-        assert test_star._p_label('4217.791Fe1_4219.893V1_16') == 0
+        assert test_star.od_date(2) == '2010-04-21T03:55:19.107'
+        with pytest.raises(KeyError):
+            test_star.od_date(3)
+
+    def testRadialVelocity(self, test_star):
+        assert pytest.approx(test_star.radialVelocity, 1.1 * u.m / u.s)
 
     def testDumpAndRestoreData(self, test_star, tmp_dir):
 
@@ -96,7 +123,7 @@ class TestStar(object):
                                           getattr(test_star, name))
             assert getattr(new_star, name).units == getattr(test_star,
                                                             name).units
-        assert new_star._obs_date_dict == test_star._obs_date_dict
-        assert new_star._transition_label_dict == test_star.\
-            _transition_label_dict
-        assert new_star._pair_label_dict == test_star._pair_label_dict
+        assert new_star._obs_date_bidict == test_star._obs_date_bidict
+        assert new_star._transition_bidict == test_star.\
+            _transition_bidict
+        assert new_star._pair_bidict == test_star._pair_bidict
