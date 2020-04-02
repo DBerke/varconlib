fd91add645ba0b8d4044a84ad1326d9ead67e1be
Daniel Berke <berke.daniel@gmail.com>
2020-04-02 11:49:14 +1100

    Made histograms non-cumulative, limited range, linear x-scale, general improvements.

--- varconlib/scripts/compare_fitting_functions.py
+++ varconlib/scripts/compare_fitting_functions.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 import unyt as u
+import sys
 
 import varconlib as vcl
 
@@ -35,28 +36,35 @@ def main():
     functions = {'uncorrected': 'Uncorrected',
                  'linear': 'Linear',
                  'quadratic': 'Quadratic',
-                 'quadratic_cross_term': 'Quad., [Fe/H]/T$_{eff}$',
-                 'quadratic_mag': r'Quad., cross term, $\mathrm{M}_{v}^2$'}
+                 'cross_term': 'Linear, [Fe/H]/T$_{eff}$',
+                 'quadratic_mag': r'Linear, cross term, $\mathrm{M}_{v}^2$'}
     files = [main_dir / f'{x}/{x}_sigmas.csv' for x in functions.keys()]
 
     fig = plt.figure(figsize=(11, 5), tight_layout=True)
     ax_pre = fig.add_subplot(1, 2, 1)
-    ax_pre.set_xscale('log')
+    # ax_pre.set_xscale('log')
     ax_pre.set_xlabel(r'Pre-fiber change $\sigma$ (m/s)')
+    ax_pre.set_xlim(left=0, right=100)
     ax_post = fig.add_subplot(1, 2, 2)
-    ax_post.set_xscale('log')
+    # ax_post.set_xscale('log')
     ax_post.set_xlabel(r'Post-fiber change $\sigma$ (m/s)')
+    ax_post.set_xlim(left=0, right=100)
+
+    bin_edges = [x for x in range(0, 1005, 3)]
 
     for file, function in zip(files, functions.keys()):
         with open(file, 'r', newline='') as f:
             data = np.loadtxt(f, delimiter=',')
-        ax_pre.hist(data[:, 1], cumulative=True, histtype='step',
-                    label=functions[function], bins='sturges')
-        ax_post.hist(data[:, 2], cumulative=True, histtype='step',
-                     label=functions[function], bins='sturges')
+        ax_pre.hist(data[:, 1], cumulative=False, histtype='step',
+                    label=functions[function], bins=bin_edges)
+        ax_post.hist(data[:, 2], cumulative=False, histtype='step',
+                     label=functions[function], bins=bin_edges)
 
-    ax_pre.legend(loc='lower right')
-    ax_post.legend(loc='lower right')
+    ax_pre.legend(loc='upper right')
+    ax_post.legend(loc='upper right')
+
+    plt.show(fig)
+    sys.exit()
 
 
 if __name__ == '__main__':
