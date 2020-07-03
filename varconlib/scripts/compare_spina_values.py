076b9cf1eae106f7774fce7ace346af0dd68472c
Daniel Berke <berke.daniel@gmail.com>
2020-07-03 17:43:23 +1000

    Add plotting of error bars to stars.

--- varconlib/scripts/compare_spina_values.py
+++ varconlib/scripts/compare_spina_values.py
@@ -110,22 +110,19 @@ casali_dict_file = vcl.data_dir / 'Casali_star_dict.pkl'
 with open(casali_dict_file, 'rb') as f:
     casali_dict = pickle.load(f)
 
-if 'HD114174' in casali_dict.values():
-    print('Found HD114174')
-
 hd_dict.update(casali_dict)
 
 sp1_stars_file = vcl.data_dir / 'SP1_Sample.csv'
 
 sp1_names = np.loadtxt(sp1_stars_file, delimiter=',', usecols=0, dtype=str)
 sp1_names = set([''.join(name.split(' ')) for name in sp1_names])
-print(sp1_names)
+# print(sp1_names)
 
 data_file = vcl.data_dir / 'Casali20_Solutions_all_nooutliers_good.csv'
 
 with open(data_file, 'r') as f:
     data = np.loadtxt(data_file, delimiter=',', skiprows=1,
-                      usecols=(0, 1, 2, 3), dtype=str)
+                      usecols=(0, 1, 2, 3, 19, 20, 21), dtype=str)
 
 names = [name for name in data[:, 0]]
 
@@ -140,6 +137,10 @@ sp1_casagrande_feh = []
 teff_list = []
 logg_list = []
 feh_list = []
+
+teff_err_list = []
+logg_err_list = []
+feh_err_list = []
 
 casagrande_teff = []
 casagrande_logg = []
@@ -166,6 +167,16 @@ for name in tqdm(names):
                     continue
                 tqdm.write(f'Matched {name} ({row[0]})')
                 num_matched_stars += 1
+                teff_list.append(float(row[1]))
+                logg_list.append(float(row[2]))
+                feh_list.append(float(row[3]))
+                teff_err_list.append(float(row[4]))
+                logg_err_list.append(float(row[5]))
+                feh_err_list.append(float(row[6]))
+                casagrande_teff.append(star.temperature.value)
+                casagrande_logg.append(star.logg)
+                casagrande_feh.append(star.metallicity)
+
                 if name in sp1_names:
                     tqdm.write('Matched SP1 star')
                     sp1_teff.append(float(row[1]))
@@ -175,13 +186,9 @@ for name in tqdm(names):
                     sp1_casagrande_logg.append(star.logg)
                     sp1_casagrande_feh.append(star.metallicity)
                     num_sp1_stars += 1
+                    if float(row[3]) > 0.2:
+                        print(f'Anomalous star is {name}')
 
-                teff_list.append(float(row[1]))
-                logg_list.append(float(row[2]))
-                feh_list.append(float(row[3]))
-                casagrande_teff.append(star.temperature.value)
-                casagrande_logg.append(star.logg)
-                casagrande_feh.append(star.metallicity)
 
 tqdm.write(f'Matched {num_matched_stars} stars.')
 tqdm.write(f'Matched {num_sp1_stars} SP1 stars.')
@@ -194,11 +201,14 @@ sp1_temperatures = np.array(sp1_casagrande_teff) - np.array(sp1_teff)
 sp1_loggs = np.array(sp1_casagrande_logg) - np.array(sp1_logg)
 sp1_metallicities = np.array(sp1_casagrande_feh) - np.array(sp1_feh)
 
+point_color = 'CornflowerBlue'
+sp1_point_color = 'DarkOrange'
+point_size = 5
+
 t_lim_lower = 5500
 t_lim_upper = 6100
 x1 = (t_lim_lower, t_lim_upper)
 y1 = np.linspace(*x1, 2)
-
 
 fig = plt.figure(figsize=(14, 8), tight_layout=True)
 ax1 = fig.add_subplot(2, 3, 1)
@@ -212,11 +222,33 @@ logg_lim_upper = 4.6
 x2 = (logg_lim_lower, logg_lim_upper)
 y2 = np.linspace(*x2, 2)
 
+ax1.errorbar(casagrande_teff, teff_list, yerr=teff_err_list,
+             linestyle='', marker='o', markersize=point_size,
+             color=point_color, markeredgecolor='Black')
+ax1.errorbar(sp1_casagrande_teff, sp1_teff,
+             linestyle='', marker='o', markersize=point_size,
+             color=sp1_point_color, markeredgecolor='Black',
+             label='SP1 stars')
+ax1.plot(x1, y1,
+         color='Black')
+ax1.legend()
+
 ax2 = fig.add_subplot(2, 3, 2)
 ax2.set_xlabel('Casagrande+2011 log(g)')
 ax2.set_ylabel('Spina+2020 log(g)')
 ax2.set_xlim(left=logg_lim_lower, right=logg_lim_upper)
 ax2.set_ylim(bottom=logg_lim_lower, top=logg_lim_upper)
+
+ax2.errorbar(casagrande_logg, logg_list, yerr=logg_err_list,
+             linestyle='', marker='o', markersize=point_size,
+             color=point_color, markeredgecolor='Black')
+ax2.errorbar(sp1_casagrande_logg, sp1_logg,
+             linestyle='', marker='o', markersize=point_size,
+             color=sp1_point_color, markeredgecolor='Black',
+             label='SP1 stars')
+ax2.plot(x2, y2,
+         color='Black')
+ax2.legend()
 
 feh_lim_lower = -0.34
 feh_lim_upper = 0.34
@@ -229,38 +261,13 @@ ax3.set_ylabel('Spina+2020 [Fe/H]')
 ax3.set_xlim(left=feh_lim_lower, right=feh_lim_upper)
 ax3.set_ylim(bottom=feh_lim_lower, top=feh_lim_upper)
 
-point_color = 'CornflowerBlue'
-sp1_point_color = 'DarkSalmon'
-
-ax1.plot(casagrande_teff, teff_list,
-         linestyle='', marker='o',
-         color=point_color, markeredgecolor='Black')
-ax1.plot(sp1_casagrande_teff, sp1_teff,
-         linestyle='', marker='o',
-         color=sp1_point_color, markeredgecolor='Black',
-         label='SP1 stars')
-ax1.plot(x1, y1,
-         color='Black')
-ax1.legend()
-
-ax2.plot(casagrande_logg, logg_list,
-         linestyle='', marker='o',
-         color=point_color, markeredgecolor='Black')
-ax2.plot(sp1_casagrande_logg, sp1_logg,
-         linestyle='', marker='o',
-         color=sp1_point_color, markeredgecolor='Black',
-         label='SP1 stars')
-ax2.plot(x2, y2,
-         color='Black')
-ax2.legend()
-
-ax3.plot(casagrande_feh, feh_list,
-         linestyle='', marker='o',
-         color=point_color, markeredgecolor='Black')
-ax3.plot(sp1_casagrande_feh, sp1_feh,
-         linestyle='', marker='o',
-         color=sp1_point_color, markeredgecolor='Black',
-         label='SP1 stars')
+ax3.errorbar(casagrande_feh, feh_list, yerr=feh_err_list,
+             linestyle='', marker='o', markersize=point_size,
+             color=point_color, markeredgecolor='Black')
+ax3.errorbar(sp1_casagrande_feh, sp1_feh,
+             linestyle='', marker='o', markersize=point_size,
+             color=sp1_point_color, markeredgecolor='Black',
+             label='SP1 stars')
 ax3.plot(x3, y3,
          color='Black')
 ax3.legend()
@@ -270,13 +277,13 @@ ax4.set_xlabel('Casagrande+2011 Temperature (K)')
 ax4.set_ylabel('(Casagrande+2011 – Spina+2020) Temperature (K)')
 ax4.set_xlim(left=t_lim_lower, right=t_lim_upper)
 
-ax4.plot(casagrande_teff, temperatures,
-         linestyle='', marker='o',
-         color=point_color, markeredgecolor='Black')
-ax4.plot(sp1_casagrande_teff, sp1_temperatures,
-         linestyle='', marker='o',
-         color=sp1_point_color, markeredgecolor='Black',
-         label='SP1 stars')
+ax4.errorbar(casagrande_teff, temperatures, yerr=teff_err_list,
+             linestyle='', marker='o', markersize=point_size,
+             color=point_color, markeredgecolor='Black')
+ax4.errorbar(sp1_casagrande_teff, sp1_temperatures,
+             linestyle='', marker='o', markersize=point_size,
+             color=sp1_point_color, markeredgecolor='Black',
+             label='SP1 stars')
 ax4.axhline(y=0, color='Black')
 ax4.legend()
 
@@ -285,13 +292,13 @@ ax5.set_xlabel('Casagrande+2011 log(g)')
 ax5.set_ylabel('(Casagrande+2011 – Spina+2020) log(g)')
 ax5.set_xlim(left=logg_lim_lower, right=logg_lim_upper)
 
-ax5.plot(casagrande_logg, loggs,
-         linestyle='', marker='o',
-         color=point_color, markeredgecolor='Black')
-ax5.plot(sp1_casagrande_logg, sp1_loggs,
-         linestyle='', marker='o',
-         color=sp1_point_color, markeredgecolor='Black',
-         label='SP1 stars')
+ax5.errorbar(casagrande_logg, loggs, yerr=logg_err_list,
+             linestyle='', marker='o', markersize=point_size,
+             color=point_color, markeredgecolor='Black')
+ax5.errorbar(sp1_casagrande_logg, sp1_loggs,
+             linestyle='', marker='o', markersize=point_size,
+             color=sp1_point_color, markeredgecolor='Black',
+             label='SP1 stars')
 ax5.axhline(y=0, color='Black')
 ax5.legend()
 
@@ -299,13 +306,13 @@ ax6 = fig.add_subplot(2, 3, 6)
 ax6.set_xlabel('Casagrande+2011 [Fe/H]')
 ax6.set_ylabel('(Casagrande+2011 – Spina+2020) [Fe/H]')
 
-ax6.plot(casagrande_feh, metallicities,
-         linestyle='', marker='o',
-         color=point_color, markeredgecolor='Black')
-ax6.plot(sp1_casagrande_feh, sp1_metallicities,
-         linestyle='', marker='o',
-         color=sp1_point_color, markeredgecolor='Black',
-         label='SP1 stars')
+ax6.errorbar(casagrande_feh, metallicities, yerr=feh_err_list,
+             linestyle='', marker='o', markersize=point_size,
+             color=point_color, markeredgecolor='Black')
+ax6.errorbar(sp1_casagrande_feh, sp1_metallicities,
+             linestyle='', marker='o', markersize=point_size,
+             color=sp1_point_color, markeredgecolor='Black',
+             label='SP1 stars')
 ax6.axhline(y=0, color='Black')
 ax6.legend()
 
