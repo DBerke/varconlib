70de077a4b240c478ebe6dd5929c5fb6e79416e4
Daniel Berke <berke.daniel@gmail.com>
2020-07-23 14:11:51 +1000

    Improved calculation of chi^2_nu, added CLI options, improved plots
    Added option to use manually-assigned bins along with equal-
    occupancy number bins, added options to use one of the four fitting
    funtions under consideration.
    
    Calculation of chi^2_nu now adjusts each bin by the weighted mean
    of that bin, to make it symmetric around zero.
    
    No longer plotting sigma, just sigma_sys, and now plots all transitions
    for a fuller picture. Only computes sigma_sys for a bin if at least 6
    points in the bin.
    
    Shifted range of temperatures in plots upwards to better capture full
    range of stellar T_effs.

--- varconlib/scripts/check_stellar_parameter_dependence.py
+++ varconlib/scripts/check_stellar_parameter_dependence.py
@@ -14,6 +14,7 @@ import argparse
 from inspect import signature
 from pathlib import Path
 import pickle
+import sys
 
 import h5py
 import hickle
@@ -45,7 +46,7 @@ def pairwise(iterable):
 
 
 def create_comparison_figure(ylims=None,
-                             temp_lims=(5300 * u.K, 6200 * u.K),
+                             temp_lims=(5400 * u.K, 6300 * u.K),
                              mtl_lims=(-0.75, 0.4),
                              logg_lims=(4.1, 4.6)):
     """Create and returns a figure with pre-set subplots.
@@ -81,6 +82,7 @@ def create_comparison_figure(ylims=None,
                   width_ratios=(5, 5, 5))
 
     temp_ax_pre = comp_fig.add_subplot(gs[0, 0])
+    temp_ax_pre.set_ylim(bottom=-1, top=80)
     temp_ax_post = comp_fig.add_subplot(gs[1, 0],
                                         sharex=temp_ax_pre,
                                         sharey=temp_ax_pre)
@@ -137,7 +139,8 @@ def create_comparison_figure(ylims=None,
     # Just label the left-most two subplots' y-axes.
     for ax, era in zip((temp_ax_pre, temp_ax_post),
                        ('Pre', 'Post')):
-        ax.set_ylabel(f'{era}-fiber change offset (m/s)')
+        ax.set_ylabel(f'{era}-fiber change '
+                      r'$\sigma_\mathrm{sys}$ (m/s)')
 
     axes_dict = {'temp_pre': temp_ax_pre, 'temp_post': temp_ax_post,
                  'mtl_pre': mtl_ax_pre, 'mtl_post': mtl_ax_post,
@@ -160,8 +163,18 @@ def main():
     vprint(f'Found {len(transitions_list)} transitions.')
 
     # Define the model to use.
-    model_func = fit.linear_model
+    if args.linear:
+        model_func = fit.linear_model
+    elif args.quadratic:
+        model_func = fit.quadratic_model
+    elif args.cross_term:
+        model_func = fit.cross_term_model
+    elif args.quadratic_magnitude:
+        model_func = fit.quadratic_mag_model
+
+    # model_func = fit.quadratic_model
     model_name = '_'.join(model_func.__name__.split('_')[:-1])
+    tqdm.write(f'Using {model_name} model.')
 
     db_file = vcl.databases_dir / f'stellar_db_{model_name}_params.hdf5'
     # Load data from HDF5 database file.
@@ -186,27 +199,70 @@ def main():
     # Handle various fitting and plotting setup:
     eras = {'pre': 0, 'post': 1}
     param_dict = {'temp': 0, 'mtl': 1, 'logg': 2}
+    plot_types = ('temp', 'mtl', 'logg')
 
     params_list = []
     # Figure out how many parameters the model function takes, so we know how
     # many to dynamically give it later.
-    for i in range(len(signature(model_func).parameters)-1):
+    num_params = len(signature(model_func).parameters)
+    for i in range(num_params - 1):
         params_list.append(0.)
 
     # Set up the figure with subplots.
     comp_fig, axes_dict = create_comparison_figure(ylims=None)
 
-    labels = ['4219.893V1_16', '4492.660Fe2_25', '4653.460Cr1_29',
-              '4767.190Mn1_33', '5138.510Ni1_42', '5200.158Fe1_43',
-              '5571.164Fe1_50', '6067.161Fe1_59', '6192.900Ni1_61']
+    if args.label:
+        labels = (args.label, )
 
-    for label in tqdm(labels):
-        # label = args.label
+    else:
+        # labels = ['4219.893V1_16', '4490.998Fe1_25', '4492.660Fe2_25',
+        #           '4500.398Fe1_25', '4589.484Cr2_28', '4653.460Cr1_29',
+        #           '4738.098Fe1_32', '4767.190Mn1_33', '4811.877Zn1_34',
+        #           '4940.192Fe1_37', '5138.510Ni1_42', '5178.000Ni1_43',
+        #           '5200.158Fe1_43', '5571.164Fe1_50', '5577.637Fe1_50',
+        #           '6067.161Fe1_59', '6123.910Ca1_60', '6144.183Si1_60',
+        #           '6155.928Na1_61', '6162.452Na1_61', '6178.520Ni1_61',
+        #           '6192.900Ni1_61']
+        labels = []
+        for transition in tqdm(transitions_list):
+            for order_num in transition.ordersToFitIn:
+                label = '_'.join([transition.label, str(order_num)])
+                labels.append(label)
 
-        # The column number to use for this transition:
-        col = column_dict[label]
+        tqdm.write(f'Analyzing {len(labels)} transitions.')
+
+    if not args.nbins:
+        bin_dict = {}
+        # Set bins manually.
+        # bin_dict[name] = np.linspace(5457, 6257, 5)
+        bin_dict['temp'] = [5377, 5477, 5577, 5677,
+                            5777, 5877, 5977, 6077,
+                            6177, 6277]
+        # bin_dict[name] = np.linspace(-0.75, 0.45, 5)
+        bin_dict['mtl'] = [-0.75, -0.6, -0.45, -0.3,
+                           -0.15, 0, 0.15, 0.3, 0.45]
+        # bin_dict[name] = np.linspace(4.1, 4.6, 5)
+        bin_dict['logg'] = [4.04, 4.14, 4.24, 4.34, 4.44, 4.54, 4.64]
 
         for time in eras.keys():
+            for plot_type, lims in zip(plot_types,
+                                       (temp_lims, mtl_lims, logg_lims)):
+                ax = axes_dict[f'{plot_type}_{time}']
+                for limit in bin_dict[plot_type]:
+                    ax.axvline(x=limit, color='Green',
+                               alpha=0.3)
+
+    for label in tqdm(labels):
+
+        vprint(f'Analyzing {label}...')
+        # The column number to use for this transition:
+        try:
+            col = column_dict[label]
+        except KeyError:
+            print(f'Incorrect key given: {label}')
+            sys.exit(1)
+
+        for time in tqdm(eras.keys()):
 
             vprint(20 * '=')
             vprint(f'Working on {time}-change era.')
@@ -270,9 +326,8 @@ def main():
 
             # Iterate over binned segments of the data to find what additional
             # systematic error is needed to get a chi^2 of ~1.
-
             arrays_dict = {name: array for name, array in
-                           zip(('temp', 'mtl', 'logg'),
+                           zip(plot_types,
                                (temps, metals, loggs))}
 
             popt, pcov = curve_fit(model_func, x_data, offsets.value,
@@ -284,25 +339,34 @@ def main():
             model_values = model_func(x_data, *popt)
             residuals = offsets.value - model_values
 
-            # Set up the bins:
-            bin_dict = {}
-            nbins = int(args.nbins)
-            for name in arrays_dict:
-
+            if args.nbins:
+                nbins = int(args.nbins)
+                # Use quantiles to get bins with the same number of elements
+                # in them.
+                vprint(f'Generating {args.nbins} bins.')
                 bins = np.quantile(arrays_dict[name],
                                    np.linspace(0, 1, nbins+1),
                                    interpolation='nearest')
                 bin_dict[name] = bins
 
+            min_bin_size = 6
             sigma_sys_dict = {}
-            for name in tqdm(arrays_dict.keys()):
+            num_params = 1
+            for name in tqdm(plot_types):
                 sigma_sys_list = []
                 sigma_list = []
                 bin_mid_list = []
                 for bin_lims in pairwise(bin_dict[name]):
                     lower, upper = bin_lims
-                    bin_mid_list.append((lower + upper)/2)
+                    bin_mid_list.append((lower + upper) / 2)
                     mask_array = ma.masked_outside(arrays_dict[name], *bin_lims)
+                    num_points = mask_array.count()
+                    vprint(f'{num_points} values in bin ({lower},{upper})')
+                    if num_points < min_bin_size:
+                        vprint('Skipping this bin!')
+                        sigma_list.append(np.nan)
+                        sigma_sys_list.append(np.nan)
+                        continue
                     temps_copy = temps[~mask_array.mask]
                     metals_copy = metals[~mask_array.mask]
                     mags_copy = mags[~mask_array.mask]
@@ -311,24 +375,46 @@ def main():
                     x_data_copy = np.stack((temps_copy, metals_copy, mags_copy),
                                            axis=0)
 
-                    n_params = len(popt)
                     chi_squared_nu = fit.calc_chi_squared_nu(residuals_copy,
                                                              errs_copy,
-                                                             n_params)
-                    sigma_sys = 0
-                    while chi_squared_nu > 1:
-                        sigma_sys += 0.01
-                        err_iter = np.sqrt(np.square(errs_copy) +
-                                           np.square(sigma_sys))
+                                                             num_params)
+                    sigma_sys_delta = 0.01
+                    sigma_sys = -sigma_sys_delta
+                    chi_squared_nu = np.inf
+                    variances = np.square(errs_copy)
+                    while chi_squared_nu > 1.0:
+                        sigma_sys += sigma_sys_delta
+                        variance_sys = np.square(sigma_sys)
+                        variances_iter = variances + variance_sys
+                        # err_iter = np.sqrt(np.square(errs_copy) +
+                        #                    np.square(sigma_sys))
+                        weights = 1 / variances_iter
+                        wmean, sum_weights = np.average(residuals_copy,
+                                                        weights=weights,
+                                                        returned=True)
 
-                        chi_squared_nu = fit.calc_chi_squared_nu(residuals_copy,
-                                                                 err_iter,
-                                                                 n_params)
+                        chi_squared_nu = fit.calc_chi_squared_nu(
+                            residuals_copy - wmean, np.sqrt(variances_iter),
+                            num_params)
 
                     sigma_sys_list.append(sigma_sys)
-                    sigma_list.append(np.std(residuals_copy))
+                    sigma = np.std(residuals_copy)
+                    sigma_list.append(sigma)
                     # tqdm.write(f'sigma_sys is {sigma_sys:.3f}')
                     # tqdm.write(f'chi^2_nu is {chi_squared_nu}')
+                    if sigma_sys / sigma > 1.2:
+                        print('---')
+                        print(bin_lims)
+                        print(mask_array)
+                        print(metals)
+                        print(residuals)
+                        print(n_params)
+                        print(num_params)
+                        print(residuals_copy)
+                        print(errs_copy)
+                        print(sigma)
+                        print(sigma_sys)
+                        sys.exit()
 
                 sigma_sys_dict[f'{name}_sigma_sys'] = sigma_sys_list
                 sigma_sys_dict[f'{name}_sigma'] = sigma_list
@@ -336,18 +422,19 @@ def main():
 
             # sigma = np.nanstd(residuals)
 
-            for plot_type, lims in zip(('temp', 'mtl', 'logg'),
+            for plot_type, lims in zip(plot_types,
                                        (temp_lims, mtl_lims, logg_lims)):
                 ax = axes_dict[f'{plot_type}_{time}']
                 ax.plot(sigma_sys_dict[f'{plot_type}_bin_mids'],
                         sigma_sys_dict[f'{plot_type}_sigma_sys'],
-                        color='Green', marker='o', alpha=0.7,
+                        color='Black', alpha=0.15,
                         label=r'$\sigma_\mathrm{sys}$')
-                ax.plot(sigma_sys_dict[f'{plot_type}_bin_mids'],
-                        sigma_sys_dict[f'{plot_type}_sigma'],
-                        color='Blue', marker='o', alpha=0.5,
-                        label=r'$\sigma$')
-                # ax.legend()
+                # ax.plot(sigma_sys_dict[f'{plot_type}_bin_mids'],
+                #         sigma_sys_dict[f'{plot_type}_sigma'],
+                #         color='Blue', alpha=0.3,
+                #         label=r'$\sigma$')
+                if args.label:
+                    ax.legend()
 
                 # ax.annotate(r'$\sigma_\mathrm{sys}$:'
                 #             f' {sys_err:.2f}',
@@ -363,8 +450,14 @@ def main():
                 #             verticalalignment='top')
                 # data = np.array(ma.masked_invalid(residuals).compressed())
 
-    file_name = Path('/Users/dberke/Pictures/'
-                     f'sigma_sys_stellar_parameter_dependance/Combined.png')
+    plot_path = Path('/Users/dberke/Pictures/'
+                     f'sigma_sys_stellar_parameter_dependance')
+    if args.label:
+        file_name = plot_path / f'{args.label}.png'
+    elif args.nbins:
+        file_name = plot_path / f'Combined_{model_name}_quantiles.png'
+    else:
+        file_name = plot_path / f'Combined_{model_name}_fixed_bins.png'
     # plt.show()
     comp_fig.savefig(str(file_name))
 
@@ -378,10 +471,20 @@ if __name__ == '__main__':
                         help='The label of the transition to plot (e.g. '
                         "'4589.484Cr2_28'.")
     parser.add_argument('--nbins', action='store', type=int,
-                        default=5,
                         help='The number of bins to use (default: 5).')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Print out more information about the script.')
+
+    func = parser.add_mutually_exclusive_group(required=True)
+    func.add_argument('--linear', action='store_true',
+                      help='Use a function linear in all three variables.')
+    func.add_argument('--quadratic', action='store_true',
+                      help='Use a function quadratic in all three variables.')
+    func.add_argument('--cross-term', action='store_true',
+                      help='Use a linear model with cross term ([Fe/H]/Teff).')
+    func.add_argument('--quadratic-magnitude', action='store_true',
+                      help='Use a cross term with quadratic magnitude.')
+
     args = parser.parse_args()
 
     vprint = vcl.verbose_print(args.verbose)
